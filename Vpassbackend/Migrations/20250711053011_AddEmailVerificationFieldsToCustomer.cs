b6a0c3bb776e16f47b8076ec530c9764498ceb2e
kin-lgtm <143951269+kin-lgtm@users.noreply.github.com>
2025-07-14 10:05:32 +0530

    update delete function

---
+++ Vpassbackend/Migrations/20250711053011_AddEmailVerificationFieldsToCustomer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vpassbackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailVerificationFieldsToCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

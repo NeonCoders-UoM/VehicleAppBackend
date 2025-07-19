ec839d8b1bcc9b90878ec08ff081e75da7e681e6
B. G. M. Prabuddhi <bgm.prabuddhi@gmail.com>
2025-07-20 01:09:59 +0530

    Create DTOs for Appointment Management

---
+++ Vpassbackend/DTOs/AppointmentDetailForCustomerDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Vpassbackend.DTOs
+{
+    public class AppointmentDetailForCustomerDTO
+    {
+        public string VehicleRegistration { get; set; } = null!;
+        public DateTime AppointmentDate { get; set; }
+        public int ServiceCenterId { get; set; }
+        public string? ServiceCenterAddress { get; set; }
+        public string? ServiceCenterName { get; set; }
+        public double? DistanceInKm { get; set; } // GPS logic
+        public double LoyaltyPoints { get; set; }
+
+        public List<AppointmentServiceDetailDTO> Services { get; set; } = new();
+        public decimal TotalCost { get; set; }
+    }
+}

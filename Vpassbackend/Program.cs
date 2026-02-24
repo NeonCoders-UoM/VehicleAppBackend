c0cc4ab1d3b5cf4e8708251aecb299c86defa478
kin-lgtm <143951269+kin-lgtm@users.noreply.github.com>
2026-02-24 16:24:42 +0530

    update program.cs

--- Vpassbackend/Program.cs
+++ Vpassbackend/Program.cs
@@ -361,4 +361,4 @@ app.MapControllers();
 // Configure for actual device access
 // Use 0.0.0.0 to listen on all network interfaces (accessible from real devices)
 // Your device should connect to http://<YOUR_COMPUTER_IP>:5039
-app.Run("http://0.0.0.0:5039");
+app.Run();
